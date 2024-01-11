fad6bec95e537f192442329825631ded1525d7ea
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-01-11 10:19:17 +0430

    Style: About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
 
 const App = () => {
   const [themeDark, setThemeDark] = useState(true);
@@ -9,6 +10,7 @@ const App = () => {
     <div className={themeDark ? "dark" : ""}>
       <Navbar setThemeDark={setThemeDark} themeDark={themeDark} />
       <Home />
+      <About />
     </div>
   );
 };
---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import aboutPic from "../assets/about-img.jpeg";
+
+const About = () => {
+  return (
+    <div className="w-full relative dark:bg-gray-900 dark:text-white p-3">
+      <div className="max-w-5xl mx-auto gap-10 flex flex-col md:flex-row h-full items-center text-center md:text-left">
+        <div>
+          <h2 className="font-bold text-3xl md:text-5xl mt-10">About me</h2>
+          <p className="text-gray-400 mt-5 leading-8 text-lg">
+            I am Mahdi Arian and I am a Graphic Designer and Content Creator
+            based in Kabul, Afghanistan. I create nice and stunning contents. I
+            spend my free times on reading.
+          </p>
+        </div>
+        <img
+          src={aboutPic}
+          alt="Mahdi Arian"
+          className="w-full md:w-80 object-cover mt-10 rounded-md shadow-md"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default About;
