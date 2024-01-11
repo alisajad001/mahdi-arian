323bc753691c4c952dd5bf43e6036a188b729059
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-01-11 10:40:58 +0430

    Style: Contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,15 @@ const Contact = () => {
       id="contact"
       className="w-full relative dark:bg-gray-900 dark:text-white p-3"
     >
-      <div className="max-w-6xl mt-20 mx-auto gap-10 flex flex-col h-full items-center">
-        asdasdsa
+      <div className="max-w-6xl mx-auto gap-10 flex flex-col h-full items-center">
+        <h2 className="font-bold text-3xl text-center md:text-5xl mt-10">
+          Let's Connect
+        </h2>
+
+        <div>
+          <p>📩 Email: mahdiarian006@gmail.com</p>
+          <p>📞 Phone: 0794681185</p>
+        </div>
       </div>
     </div>
   );
