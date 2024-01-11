ad743b9cbb7ff8fbc116453e69dcd5d6d5b57a89
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-01-11 10:46:17 +0430

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-white px-3 py-5 dark:bg-gray-900">
+      <p className="text-gray-700 dark:text-gray-400 text-sm text-center">
+        Coded with ❤ by{" "}
+        <a href="https://alisajadahmadi.netlify.app">Ali Sajad</a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
